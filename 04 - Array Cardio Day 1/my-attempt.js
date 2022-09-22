e1d1a8c9f055b35934b8642234705fd17330da8d
Oscar Elmahdy <opelm1@gmail.com>
2022-09-22 17:40:26 +0800

    Move js to external file

--- 04 - Array Cardio Day 1/my-attempt.js
+++ 04 - Array Cardio Day 1/my-attempt.js
@@ -63,29 +63,96 @@ const people = [
 ];
 
 // Array.prototype.filter()
-// 1. Filter the list of inventors for those who were born in the 1500's
+console.log(
+  "1. Filter the list of inventors for those who were born in the 1500's"
+);
+
 const fifteens = inventors.filter((person) => {
   if (person.year >= 1500 && person.year <= 1599) {
     return person;
   }
 });
+console.table(fifteens);
 
 // Array.prototype.map()
-// 2. Give us an array of the inventors first and last names
+console.log("\n2. Give us an array of the inventors first and last names");
+
+const inventorsNames = inventors.map((person) => {
+  return `${person.first} ${person.last}`;
+});
+
+console.table(inventorsNames);
 
 // Array.prototype.sort()
-// 3. Sort the inventors by birthdate, oldest to youngest
+console.log("\n3. Sort the inventors by birthdate, oldest to youngest");
+
+const inventorsSortYear = inventors.sort((a, b) => {
+  // return a.year < b.year ? 1 : -1;
+  return a.year < b.year;
+});
+
+console.table(inventorsSortYear);
 
 // Array.prototype.reduce()
-// 4. How many years did all the inventors live all together?
+console.log("\n4. How many years did all the inventors live all together?");
+
+const inventorsTotalYearsLived = inventors.reduce((acc, cur) => {
+  return acc + (cur.passed - cur.year);
+}, 0);
+
+console.log(inventorsTotalYearsLived);
 
 // 5. Sort the inventors by years lived
+console.log("\n5. Sort the inventors by years lived");
 
-// 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
+const inventorsSortYearsLived = inventors.sort((a, b) => {
+  a.lived = a.passed - a.year;
+  b.lived = b.passed - b.year;
+
+  return a.lived < b.lived;
+});
+
+console.table(inventorsSortYearsLived);
+
+console.log(
+  "\n6. create a list of Boulevards in Paris that contain 'de' anywhere in the name\n(done in console already)"
+);
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+// copy into console:
 
-// 7. sort Exercise
-// Sort the people alphabetically by last name
+// const de = [...document.querySelectorAll(".mw-category a")]
+// const de = Array.from(document.querySelectorAll(".mw-category a"))
+//   .map((a) => a.textContent)
+//   .filter((text) => text.includes("de"));
+
+// console.table(de);
+
+// 7. Sort Exercise
+console.log("\n7. Sort the people alphabetically by last name");
+
+const peopleSorted = people.sort((a, b) => {
+  return a > b;
+});
+
+console.table(peopleSorted);
+
+// 7.1 Sort (Oscar)
+console.log(
+  "\n7.1. Sort the people alphabetically by first name, displayed as FirstName LastName "
+);
+
+// const peopleSplit = people.split(", ");
+const peopleFirstLast = [];
+
+for (let i = 0; i < people.length; ++i) {
+  let personSplit = people[i].split(", ");
+
+  peopleFirstLast[i] = `${personSplit[1]} ${personSplit[0]}`;
+}
+
+const peopleSortedByFirst = peopleFirstLast.sort((a, b) => a > b);
+
+console.table(peopleSortedByFirst);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
@@ -105,3 +172,21 @@ const data = [
   "car",
   "truck",
 ];
+
+// Without using .reduce:
+const transportation = {};
+
+for (let i = 0; i < data.length; ++i) {
+  // let iContent = data[i];
+  if (transportation[data[i]]) {
+    ++transportation[data[i]];
+  } else {
+    transportation[data[i]] = 1;
+  }
+}
+
+console.table(transportation);
+
+// Using .reduce:
+// currently can't do it like WesBos. todo later.
+const transSums = data.reduce((acc, cur) => {});
